0d88922f9352d8a67d1e52faa031c07a380892a8
Trey <trey.hambrick@cox.net>
2023-10-04 20:55:46 -0400

    first commit123

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -37,13 +37,13 @@
     <li>git commit -m "first commit123"</li>
     <li>git branch -M main</li>
     <li>git remote rm origin</li>
-    <li>git remote add origin https://github.com/treyhambrick/club.git</li>
+    <li>git remote add origin git@github.com:treyhambrick/club.git</li>
     <li>git push -u origin main</li>
     <li>git add dist && git commit -m "Initial dist subtree commit"</li>
     <li>git subtree push --prefix dist origin gh-pages</li>
     <li><U>Note:</U>wait 7 minutes the goto: https://treyhambrick.github.io/club/  (refresh page to see changes). </li>
     <li><U>Note:</U>  Remove the dist directory from the project’s .gitignore file</li>
-    <li>On a Mac: git remote add origin git@github.com:treyhambrick/club.git</li>
+    <li>On a PC: git remote add origin https://github.com/treyhambrick/club.git</li>
 
     <!---li>maybe before run try:  git remote set-url origin https://treyhambrick.github.io/club/</li--->
    </ul>
